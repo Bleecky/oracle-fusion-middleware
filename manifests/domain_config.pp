ea980632fd537ac5262ce15f1145b1d9031a21d6
Peter Bleeck <peter.bleeck@os.uk>
2015-03-23 14:02:49 +0000

    Updated template to loop through yaml properties Added example YAML syntax to domain_config.pp

--- manifests/domain_config.pp
+++ manifests/domain_config.pp
@@ -1,7 +1,23 @@
+# Example YAML Syntax for data source configuration
+# ---
+# weblogic:
+#   datasources:
+#   -
+#     name: bleecky1
+#     host: host1
+#     sid:  db1
+#     user: user1
+#     pwd:  s3cr3t1
+#   -
+#     name: bleecky2
+#     host: host2
+#     sid:  db2
+#     user: user2
+#     pwd:  s3cr3t2
+
 class oracle_fusion_middleware::domain_config { 
     $ds_list = 'datasource1'
     $config = hiera_hash('weblogic')
-    $ds_config = $config['datasources']['name']
     
     file {'/tmp/oracle/ds.properties': 
     ensure  => present,
